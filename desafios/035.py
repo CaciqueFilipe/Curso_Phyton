404a450847d01f77f70e4f3652c0aff6d6739b6b
Filipe Cacique <fcacique4@gmail.com>
2026-05-06 14:33:08 -0300

    correcao ex 035

--- desafios/035.py
+++ desafios/035.py
@@ -4,10 +4,10 @@ a = int(input('Digite o comprimento da primeira reta: '))
 b = int(input('Digite o comprimento da segunda reta: '))
 c = int(input('Digite o comprimento da terceira reta: '))
 
-if (a + b) > c:
-    if (a + c) > b:
-        if (b + c) > a:
-            print('Consigo formar um triangulo')
-        else:
-            print('Não dá para formar um triangulo')
+
+
+if (a + b) > c and (a + c) > b and (b + c) > a:
+    print('Consigo formar um triangulo')
+else:
+    print('Não dá para formar um triangulo')
 
