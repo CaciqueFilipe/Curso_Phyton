b1ad2434344020336e3668bdb40544b9ae4477d3
Filipe Cacique <fcacique4@gmail.com>
2026-05-06 09:42:06 -0300

    correcao exercicios

--- desafios/022.py
+++ desafios/022.py
@@ -9,3 +9,18 @@ print("Seu nome com todas letras maisculas: {}".format(nome.upper()))
 print("Seu nome com todas letras minusculas: {}".format(nome.lower()))
 print("Seu nome tem ao todo {} letras".format(len(nome) - nome.count(' ')))
 print("Seu primeiro nome tem {} letras".format(nome.find(' ')))
+
+'''
+## Outra opção sugerida pelo Gustavo:
+
+print("\n{:=^20}".format(" Gustavo "))
+nome = str(input("Digite o seu nome completo:")).strip()
+print('Analisando seu nome...')
+print("Seu nome em maisculas: {}".format(nome.upper()))
+print("Seu nome em minusculas: {}".format(nome.lower()))
+print("Seu nome tem ao todo {} letras".format(len(nome) - nome.count(' ')))
+print("Seu primeiro nome tem {} letras".format(nome.find(' ')))
+#Outra opção
+separa = nome.split()
+print("Seu primeiro nome é {} e ele tem {} letras".format(separa[0], len(separa[0])))
+'''
--- desafios/027.py
+++ desafios/027.py
@@ -10,3 +10,14 @@ nomes = nome.split()
 quantidade = len(nomes)
 print('Primeiro nome: {}'.format(nomes[0]))
 print('Ultimo nome: {}'.format(nomes[quantidade - 1]))
+
+'''
+## Outra opção sugerida pelo Gustavo:
+
+print("\n{:=^20}".format(" Gustavo "))
+n = str(input("Digite o seu nome completo:")).strip()
+nome = n.split()
+print("Muito prazer em te conhecer!")
+print("Seu primeiro nome é {}".format(nome[0]))
+print("Seu ultimo nome é {}".format(nome[len(nome) - 1]))
+'''
